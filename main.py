0ab28238c066be924b844508fc98169a29f7a39f
Сергей Антонов <antonovseantonoff@yandex.ru>
2022-11-12 22:53:47 +0300

    Fulfilled the recommendations

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from urllib.parse import urlparse, unquote
 from dotenv import load_dotenv
 
 
-def get_url_server(token):
+def get_server_url(token, group_id):
     params = {
         "access_token": token,
         "v": "5.131",
-        "group_id": "217042661",
+        "group_id": group_id
     }
     response = requests.post(
         "https://api.vk.com/method/photos.getWallUploadServer", params=params
@@ -20,7 +20,7 @@ def get_url_server(token):
     return response.json()
 
 
-def gets_random_url_comic():
+def get_random_comic_url():
     response = requests.get("https://xkcd.com/info.0.json")
     response.raise_for_status()
     random_number = random.randint(1, response.json()["num"])
@@ -31,38 +31,38 @@ def gets_random_url_comic():
     return random_response.json()
 
 
-def get_name_file(url):
+def get_file_name(url):
     comic_url = unquote(url, encoding="utf-8", errors="replace")
     url_component = urlparse(comic_url).path
     return pathlib.PurePath(url_component).name
 
 
-def save_file_comic(url):
-    directory_path = os.path.join(os.path.dirname(__file__), "images/")
+def creates_directory():
+    directory_path = Path.cwd() / "images"
     Path(directory_path).mkdir(parents=True, exist_ok=True)
-    name_file = get_name_file(url)
-    response = requests.get(url)
+
+
+def save_comic_file(url, file_name):
+    response = requests.get(url["img"])
     response.raise_for_status()
-    with open(f"{directory_path}{name_file}", "wb") as file:
+    with open(Path.cwd() / "images" / f"{file_name}", "wb") as file:
         file.write(response.content)
 
 
-def uploading_server_comic(token):
-    name_file = os.listdir("images/")[-1]
-    with open(f"images/{name_file}", "rb") as file:
-        server_url = get_url_server(token)["response"]["upload_url"]
+def download_server_comic(server_url):
+    name_file = os.listdir("images")[0]
+    with open(Path.cwd() / "images" / f"{name_file}", "rb") as file:
         file = {'photo': file}
         response = requests.post(server_url, files=file)
-        response.raise_for_status()
-        return response.json()
+    response.raise_for_status()
+    return response.json()
 
 
-def saves_photo_server(token):
-    decoded_response = uploading_server_comic(token)
+def save_photo_server(token, group_id, decoded_response):
     params = {
         "access_token": token,
         "v": "5.131",
-        "group_id": "217042661",
+        "group_id": group_id,
         "server": decoded_response["server"],
         "photo": decoded_response["photo"],
         "hash": decoded_response["hash"],
@@ -74,37 +74,48 @@ def saves_photo_server(token):
     return response.json()
 
 
-def publishes_comic(token):
-    alt = gets_random_url_comic()["alt"]
-    group_id = -217042661
-    owner_id = saves_photo_server(token)["response"][0]["owner_id"]
-    media_id = saves_photo_server(token)["response"][0]["id"]
+def publish_comic(token, group_id, alt, decoded_response):
     params = {
         "access_token": token,
         "v": "5.131",
-        "message": alt,
-        "owner_id": group_id,
+        "message": alt["alt"],
+        "owner_id": f"-{group_id}",
         "from_group": 1,
-        "media_id": media_id,
-        "attachments": f"photo{owner_id}_{media_id}"
+        "media_id": decoded_response["id"],
+        "attachments":
+            f"photo" f"{decoded_response['owner_id']}_{decoded_response['id']}"
     }
     response = requests.post(
         "https://api.vk.com/method/wall.post",
         params=params
     )
     response.raise_for_status()
-    for file_comic in os.listdir("images/"):
-        os.remove(f"images/{file_comic}")
+
+
+def deletes_file():
+    os.remove(Path.cwd() / "images" / os.listdir("images")[0])
 
 
 def main():
     load_dotenv()
+    creates_directory()
+    vk_group_id = os.getenv("VK_GROUP_ID")
     vk_token = os.getenv("VK_TOKEN")
-    comic = gets_random_url_comic()["img"]
-    save_file_comic(comic)
-    uploading_server_comic(vk_token)
-    saves_photo_server(vk_token)
-    publishes_comic(vk_token)
+    random_comic_response = get_random_comic_url()
+    file_name = get_file_name(random_comic_response["img"])
+    server_url = get_server_url(vk_token, vk_group_id)["response"][
+        "upload_url"]
+    try:
+        save_comic_file(random_comic_response, file_name)
+        server_comic_response = download_server_comic(server_url)
+        decoded_response = save_photo_server(
+            vk_token, vk_group_id, server_comic_response
+        )["response"][0]
+        publish_comic(
+            vk_token, vk_group_id, random_comic_response, decoded_response
+        )
+    finally:
+        deletes_file()
 
 
 if __name__ == '__main__':
